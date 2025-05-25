19ba2ce472ec8ccf468268807c7852165359810e
sabbir53rahman <sabbir53rahman@gmail.com>
2025-05-25 08:33:06 +0600

    sdg

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./(home)/home/page";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 function page() {
   return (
     <div>
+      <Navbar/>
       <Home />
       <Footer/>
     </div>
